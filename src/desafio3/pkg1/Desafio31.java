1971b1d124dd86d7ecd01428689b8ac0c87c863e
faakw <filipjp12345@gmail.com>
2025-03-20 21:42:19 -0300

    Update Desafio31.java

--- src/desafio3/pkg1/Desafio31.java
+++ src/desafio3/pkg1/Desafio31.java
@@ -25,8 +25,7 @@ public class Desafio31 {
             peso = (72.7 * altura) - 58;
             System.out.println("Seu peso ideal é " + decimal.format(peso));
             if (sexo.equals("F")) {
-                if (sexo.equals("f")) {
-                    peso = (62.1 * altura) - 44.7;
+                peso = (62.1 * altura) - 44.7;
                     System.out.println("Seu peso ideal é " + decimal.format(peso));
                 }
             }
